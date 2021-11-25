02c01c9f3b415124e3d93a630633ade3a24ead7a
Matota <72810000+mat0ta@users.noreply.github.com>
2021-11-25 22:15:43 +0100

    Update laberinto.py

--- laberinto.py
+++ laberinto.py
@@ -4,9 +4,14 @@ laberinto = []
 
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 
-laberinto.append(([' ']*5))
-print(laberinto)
+for i in range(5):
+    laberinto.append(([' ']*5))
 
-# for i in muro:
-#     for i in range(len(muro)):
-#         print(i)
+for i in muro:
+    laberinto[int(i[0])][int(i[1])] = 'x'
+laberinto[4][4] = 's'
+print(laberinto[0])
+print(laberinto[1])
+print(laberinto[2])
+print(laberinto[3])
+print(laberinto[4])
